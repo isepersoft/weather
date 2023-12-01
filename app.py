2a1988bddc28a55d3dbe427ff20702a4dbd72f15
Pavel Perepelkin <isepersoft@gmail.com>
2023-12-01 15:37:43 +0700

    Просто еще одна правка Не относиться к программе Просто удалил коммент

--- app.py
+++ app.py
@@ -34,25 +34,6 @@ button = Button(root, text='Нажми чтобы узнать', font=40, comman
 button.pack(side=BOTTOM, pady=40)
 
 
-# city = 'Томск'
-
-# url = ('https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=metric&lang=ru&APPID=4e04ad39dd9272d1ae0b831946a29bc4')
-
-# weather_data = requests.get(url).json()
-# weather_data_structure = json.dumps(weather_data, indent=2)
-
-# temperature = round(weather_data['main']['temp'])
-# temperature_feels = round(weather_data['main']['feels_like'])
-# wind_speed = round(weather_data['wind']['speed'])
-
-# print('Сейчас в городе', city, str(temperature), 'градусов')
-# print('Ощущается как', str(temperature_feels), 'градусов')
-# print('Скорость ветра', str(wind_speed), 'м/с')
-
-# temperature_label = Label(root, text='Сейчас в городе', city, str(temperature), 'градусов', font=35)
-# temperature_feels_label = Label(root, text='Ощущается как', str(temperature_feels), 'градусов', font=35)
-# wind_speed_label = Label(root, text='Скорость ветра', str(wind_speed), 'м/с')
-
 root.mainloop()
 
 # Видео в помощь
